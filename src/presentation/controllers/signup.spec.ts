e0dc84cadfa86a8fc28e6dfffebbb090b0c6583b
jacquesmarques <jmarques1@gmail.com>
2023-05-03 08:05:57 -0300

    feat: ensure SingUpController return 400 if passsword confirmation fails

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -74,6 +74,21 @@ describe('SigUp Controller', () => {
       body: {
         name: 'any_name',
         email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'invalid_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('passwordConfirmation'))
+  })
+
+  test('Should return 400 if no confirmation password fails ', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
         password: 'any_password'
       }
     }
